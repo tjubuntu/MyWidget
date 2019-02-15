621ee4131a0204400a48673ca1a0f299378526eb
tjubuntu <tjubuntu@foxmail.com>
2019-02-15 14:30:30 +0800

    项目初始化

---
+++ WebFilesDownload/Models/FileEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebFilesDownload.Models
+{
+    public class FileEntity
+    {
+        public string fileUrl { get; set; }
+        public string fileName { get; set; }
+        public string savePath { get; set; }
+    }
+}
